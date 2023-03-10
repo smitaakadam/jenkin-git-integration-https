09dd95c655e0d7f7a7474d8064c4a28682907679
smitaakadam <smitakadam621@gmail.com>
2023-03-10 12:59:33 +0530

    first commit

---
+++ src/main/java/com/jbk/JenkinGitIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.jbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinGitIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinGitIntegrationApplication.class, args);
+	}
+
+}
